7392a58a248a9077e4dcd9579f4e8cd29a1efd24
ningj <jiangning_ricky@163.com>
2020-04-19 23:05:31 +0800

    First commit of JPlatform

---
+++ src/test/java/com/ricky/j/platform/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricky.j.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
